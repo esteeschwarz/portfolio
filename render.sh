8ca09919671cbcb715eb3422997a6ab8d828e248
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2026-05-08 08:03:56 +0200

    rm render posts

--- render.sh
+++ render.sh
@@ -28,18 +28,18 @@ set -euo pipefail
 
 Rscript -e "renv::activate(profile = 'default')"
 Rscript -e "renv::restore()"
-quarto render posts/2024-12-30-quarto-github-pages
-quarto render posts/2025-04-21-quarto-revealjs-tabset-pdf
-quarto render posts/2025-05-19-quarto-codespaces
-quarto render posts/2025-10-20-quarto-wizard-1-0-0
-quarto render posts/2025-11-06-quarto-extensions-lua
-quarto render posts/2025-11-20-quarto-editor-settings
-quarto render posts/2025-12-12-quarto-extensions-updater
-quarto render posts/2026-01-12-quarto-wizard-2-0-0
-quarto render posts/2026-01-19-typst-document-dispatcher
-quarto render posts/2026-02-27-typst-template-tutorial-part1
-quarto render posts/2026-03-05-typst-template-tutorial-part2
+# quarto render posts/2024-12-30-quarto-github-pages
+# quarto render posts/2025-04-21-quarto-revealjs-tabset-pdf
+# quarto render posts/2025-05-19-quarto-codespaces
+# quarto render posts/2025-10-20-quarto-wizard-1-0-0
+# quarto render posts/2025-11-06-quarto-extensions-lua
+# quarto render posts/2025-11-20-quarto-editor-settings
+# quarto render posts/2025-12-12-quarto-extensions-updater
+# quarto render posts/2026-01-12-quarto-wizard-2-0-0
+# quarto render posts/2026-01-19-typst-document-dispatcher
+# quarto render posts/2026-02-27-typst-template-tutorial-part1
+# quarto render posts/2026-03-05-typst-template-tutorial-part2
 
 quarto render
 
-bash posts/2025-04-21-quarto-revealjs-tabset-pdf/assets/featured.sh
+# bash posts/2025-04-21-quarto-revealjs-tabset-pdf/assets/featured.sh
